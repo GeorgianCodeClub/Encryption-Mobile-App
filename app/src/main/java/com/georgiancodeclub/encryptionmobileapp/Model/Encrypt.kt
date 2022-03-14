e7a76ddcf0210735dbc3aad4e8c1e0e2366611f4
Jacob Graham <jakegraham54@gmail.com>
2022-03-13 23:58:15 -0400

    Added boilerplate files for each of the Model, View, and Controller folders

---
+++ app/src/main/java/com/georgiancodeclub/encryptionmobileapp/Model/Encrypt.kt
@@ -0,0 +1,4 @@
+package com.georgiancodeclub.encryptionmobileapp.Model
+
+class Encrypt {
+}
---
+++ app/src/main/java/com/georgiancodeclub/encryptionmobileapp/View/IEncryptMessageView.kt
@@ -0,0 +1,4 @@
+package com.georgiancodeclub.encryptionmobileapp.View
+
+interface IEncryptMessageView {
+}
