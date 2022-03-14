2a861d6baecec38de5fe0c709c3feeacb0ae26a1
Jacob Graham <jakegraham54@gmail.com>
2022-03-14 00:03:30 -0400

    Enabled viewBinding in the application module level build.gradle. Set the content view in MainActivity to be the view binding of the default XML layout for easier and deeper access to everything defined in the XML document

--- app/src/main/java/com/georgiancodeclub/encryptionmobileapp/MainActivity.kt
+++ app/src/main/java/com/georgiancodeclub/encryptionmobileapp/MainActivity.kt
@@ -2,13 +2,15 @@ package com.georgiancodeclub.encryptionmobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.georgiancodeclub.encryptionmobileapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var mainActivityBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainActivityBinding.root)
     }
 }
